8eb84ef68bdbd9493ee17de68f2e1691f57b36f3
Ville Herranen <ville@partagames.com>
2015-11-21 23:51:47 +0200

    Add an awesome server implementation

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+var server = new WebSocketServer({ port: 8080 });
 
-  ws.send('something');
+var clients = []
+
+var cards = [
+	{name: "Heh"},
+	{name: "Juu"},
+	{name: "Kjeh"},
+	{name: "Rengas"}
+]
+
+server.on('connection', function connection(ws) {
+
+	clients.push(ws);
+
+	if (clients.length == 2) {
+		gameStart(clients);
+		clients = []
+	} else {
+	}
+	ws.on('message', incoming);
+
+	ws.send('something');
 });
+
+function incoming(message) {
+	console.log('received: %s', message);
+}
+
+function gameStart(clients) {
+	clients[0].send({type: "gameStart", cards: cards});
+	clients[1].send({type: "gameStart", cards: cards});
+}
