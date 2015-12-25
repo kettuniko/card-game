be3e69c839ca650b79fea5ce584909f111da7906
Niko Kettunen <niko.kettunen@reaktor.fi>
2015-12-25 14:24:31 +0200

    Refactor game.js

--- app/pages/game.js
+++ app/pages/game.js
@@ -2,10 +2,28 @@ import React from 'react'
 import Bacon from 'baconjs'
 import socketIO from 'socket.io-client'
 const io = socketIO('localhost:8081')
-const log = console.log.bind(console)
 
-export const pagePath = '/'
-export const pageTitle = 'Game'
+const log = console.log.bind(console)
+const parseJson = raw => JSON.parse(raw)
+
+const events = eventName => Bacon.fromEvent(io, eventName)
+const connects = events('connect')
+const disconnects = events('disconnect')
+const errors = events('error')
+const gameStarts = events('game-start')
+const turns = events('turn')
+
+connects.onValue(() => log('connected to server'))
+disconnects.onValue(() => log('disconnected from server'))
+errors.onValue((err) => log('error', err))
+turns.onValue((msg) => log('turn', msg))
+
+const gameStateChanges = gameStarts
+  .merge(turns)
+  .map(parseJson)
+  .map('.gameState')
+
+const playCard = card => () => io.emit('play-card', {card: card})
 
 export const initialState = {
   message: 'waiting for other player',
@@ -17,37 +35,15 @@ export const renderPage = applicationState =>
     <p id="message">{applicationState.message}</p>
     <div id="game-area"></div>
     <div id="my-cards">
-      {applicationState.players[0].cards}
+      {applicationState.players[0].cards.map(card =>
+        <img key={card.name} src={`img/${card.name}.png`} onClick={playCard(card.name)} />
+      )}
     </div>
   </body>
-
-const events = eventName => Bacon.fromEvent(io, eventName)
-const parseJson = raw => JSON.parse(raw)
-
-const connects = events('connect')
-const disconnects = events('disconnect')
-const errors = events('error')
-const gameStarts = events('game-start')
-const turns = events('turn')
-
-connects.onValue(() => log('connected to server'))
-disconnects.onValue(() => log('disconnected from server'))
-errors.onValue((err) => log('error', err))
-
-const imageNamesToElements = gameState => Object.assign(gameState, mapAllCardsToElements(gameState))
-const mapAllCardsToElements = gameState => Object.assign(gameState, {players: gameState.players.map(playerCardsToElements)})
-const playerCardsToElements = player => Object.assign(player, {cards: player.cards.map(cardToElement)})
-const cardToElement = card => <img key={card.name} src={`img/${card.name}.png`} onClick={playCard(card.name)} />
-const playCard = card => () => io.emit('play-card', {card: card})
-
-const gameStateChanges = gameStarts
-  .merge(turns)
-  .map(parseJson)
-  .map('.gameState')
-  .map(imageNamesToElements)
-
-turns.onValue((msg) => log('turn', msg))
 
 export const applicationStateProperty = initialState => gameStateChanges
   .toProperty(initialState)
   .doLog('app state')
+
+export const pagePath = '/'
+export const pageTitle = 'Game'
